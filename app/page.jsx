59a621307300d1a3c918df9957a114a7f64498a5
skociuba <skociuba83@gmail.com>
2024-02-14 16:53:52 +0100

    fixes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import {getServerSession} from 'next-auth';
 
 import User from './../components/User';
-import {authOptions} from './api/auth/[...nextauth]/route';
+import {authOptions} from './../lib/authOptions';
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
