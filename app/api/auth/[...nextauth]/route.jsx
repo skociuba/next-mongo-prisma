4eac3cc1abcb0dc46fc3e179504d2c8f0a7c410f
skociuba <skociuba83@gmail.com>
2024-02-14 16:11:47 +0100

    fix

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth/next';
+import NextAuth from 'next-auth';
 import {PrismaAdapter} from '@next-auth/prisma-adapter';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
